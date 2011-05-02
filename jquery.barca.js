4bf464bba8679ffe466ab2a4dc2d312784f242cc
aligo <aligo_x@163.com>
2011-05-02 14:10:46 +0800

    cleared code up

--- jquery.barca.js
+++ jquery.barca.js
@@ -6,44 +6,13 @@
  * Dual licensed under the MIT or GPL Version 2 licenses.
  */
 
-(function($, undefined){
+(function($, undefined) {
 
-$.extend({
+var barca = {
 
-    barcaStatus : {},
-
-    barcaSetup : function ( target, settings ) {
-
-		if ( !settings ) {
-			settings = target
-			target = $.extend( true, $.barcaSettings, settings )
-		} else {
-			$.extend( true, target, $.barcaSettings, settings )
-		}
-
-		for( var field in { context: 1, url: 1 } ) {
-			if ( field in settings ) {
-				target[ field ] = settings[ field ]
-			} else if( field in $.barcaSettings ) {
-				target[ field ] = $.barcaSettings[ field ]
-			}
-		}
-
-        if ( $.isFunction(target.url) ) {
-            target.href = target.url()
-        }else{
-            target.href = target.url
-        }
-        var href = target.url.split('#')
-        target.url = href[0]
-        target.hash = href[1] || undefined
-
-		return target
-	},
-
-    barcaSettings : {
+    settings : {
         loading : 'loading...',
-        barcaOriginal : function() {
+        original : function() {
             var origin = $('body').html()
             return {
               callback : function() {
@@ -51,9 +20,47 @@ $.extend({
               }
             }
         },
-        hashchange : function( new_hash, old_hash ) {
-        }
+        hashchange : function( new_hash, old_hash ) { }
     },
+
+    stack : {},
+
+    state : 1,
+
+    inited : false,
+
+}
+
+var getValueOrCall = function ( target ) {
+    return ( $.isFunction(target) ) ? target() : target
+}
+
+$.extend({
+
+    barcaSetup : function ( target, settings ) {
+
+		if ( !settings ) {
+			settings = target
+			target = $.extend( true, barca.settings, settings )
+		} else {
+			$.extend( true, target, barca.settings, settings )
+		}
+
+		for( var field in { context: 1, url: 1 } ) {
+			if ( field in settings ) {
+				target[ field ] = settings[ field ]
+			} else if( field in barca.settings ) {
+				target[ field ] = barca.settings[ field ]
+			}
+		}
+
+        target.href = getValueOrCall( target.url )
+        var href = target.href.split('#')
+        target.url = href[0]
+        target.hash = href[1] || undefined
+
+		return target
+	},
 
 
     barca : function( url, options ) {
@@ -75,38 +82,35 @@ $.extend({
             var args = arguments
             var state_id = window.history.state
 
-            $.barcaStatus[state_id] = {
+            barca.stack[state_id] = {
                 callback : function() {
                     success.apply(this, args)
                     s.hashchange(s.hash, old_hash)
                 }
             }
-            $.barcaStatus[state_id].callback()
+            barca.stack[state_id].callback()
 
             window.history.replaceState(state_id, document.title, window.location.href)
 
-            if (window._gaq){
-                _gaq.push(['_trackPageview'])
-            }
+            if (window._gaq) _gaq.push(['_trackPageview'])
         }
 
-        var xhr = $.barca.xhr
+        if ( !barca.inited ) {
+            barca.inited = true
+            barca.stack[1] = s.original()
+            window.history.replaceState(1, document.title, window.location.href)
+        }
+        barca.state ++
+        window.history.pushState(barca.state, s.loading, s.href)
+        document.title = s.loading
+
+        var xhr = barca.xhr
         if ( xhr && xhr.readyState < 4) {
             xhr.onreadystatechange = $.noop
             xhr.abort()
         }
 
-        $.barca.xhr = $.ajax(s)
-
-        if ( !$.barca.active ) {
-            $.barca.state = 1
-            $.barca.active = true
-            $.barcaStatus[1] = s.barcaOriginal()
-            window.history.replaceState(1, document.title, window.location.href)
-        }
-        $.barca.state ++
-        window.history.pushState($.barca.state, s.loading, s.href)
-        document.title = s.loading
+        barca.xhr = $.ajax(s)
     }
 })
 
@@ -116,8 +120,8 @@ if ( $.event.props.indexOf('state') < 0 ) $.event.props.push('state')
 $(window).bind('popstate', function( event ) {
     var state_id = event.state
     if ( state_id ) {
-        $.barca.state = state_id
-        var state = $.barcaStatus[state_id]
+        barca.state = state_id
+        var state = barca.stack[state_id]
         if ( state && state.callback ) {
             state.callback()
             succ = true
